1000796243bd8ea5ca2c1a83307967bfb69388c8
SZahitAcar <zahitacar@yandex.com>
2021-11-01 15:38:04 +0300

    first lesson

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const dollarYesterday = 9.2;
+
+console.log(dollarToday);
+const dollarToday = 9.3;
+console.log(dollarYesterday);
+
+const listem = ["haha", "leman", "teman"];
+listem[1] = "kelam";
+console.log(listem[1]);
